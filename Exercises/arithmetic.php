6c4811eb835a70941fb8746812956199d343adb4
Heidee Sia <heideesia23@yahoo.com>
2017-07-06 17:52:48 +0800

    Initial 101

--- Exercises/arithmetic.php
+++ Exercises/arithmetic.php
@@ -13,30 +13,36 @@
             <option value="divide">/</option>
         </select>
         <input type="number" name="no2">
-        <button type="submit">Compute</button>
+        <button type="submit" name="submit">Compute</button>
     </form>
     <?php
-        $number1= $__GET['no1'];
-        $number2= $__GET['no2'];
+        if (isset($_POST['submit'])){
+        $number1= $_GET['no1'];
+        $number2= $_GET['no2'];
         $arithmetic= $_GET['symbol'];
-    if ($arithmetic == "add") {
-        $total= $number1 + $number2;
-    }
-    elseif ($arithmetic == "minus") {
-        $total= $number1 - $number2;
-    }
-    elseif ($arithmetic == "multiply") {
-        $total= $number1 * $number2;
-    }
-    elseif ($arithmetic == "divide") {
-        if($number1 < $number2) {
-            $total = $number1 / $number2;
-        }
-        else {
-            
-        }
-    }
 
+            if ($arithmetic == "add") {
+                $total= $number1 + $number2;
+            }
+            elseif ($arithmetic == "minus") {
+                $total= $number1 - $number2;
+            }
+            elseif ($arithmetic == "multiply") {
+                $total= $number1 * $number2;
+            }
+            elseif ($arithmetic == "divide") {
+                if($number1 < $number2) {
+                    $total = $number1 / $number2;
+                }
+                else {
+
+                }
+            }
+            else {
+                die();
+            }
+            echo "Output: ". $total;
+        }
     ?>
 </body>
 </html>
